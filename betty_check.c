f2767c57401c3a61d4065b781aeb505d0d4a7461
Uzor <uzorgodsboy1@gmail.com>
2023-11-14 16:48:59 -0800

    betty_check

---
+++ betty_check.c
@@ -0,0 +1,18 @@
+#include "shellheader.h"
+
+/**
+ * main - Code that passes betty check
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		printf("%c\n", alph);
+	}
+	return (0);
+}
